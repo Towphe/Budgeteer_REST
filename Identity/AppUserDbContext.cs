11924599e1a728c1b3d213b3b4ce32bd565707ed
Towphe <castillochristopherjames@gmail.com>
2022-01-31 09:50:19 +0800

    Made Identity DB Context

---
+++ Identity/AppUserDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Budgeteer_REST.Identity
+{
+    public class AppUserDbContext : IdentityDbContext<AppUser>
+    {
+        public AppUserDbContext(DbContextOptions<AppUserDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
