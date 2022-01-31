793e3c1a1171c20377fe13f8f5f8440efec51061
Towphe <castillochristopherjames@gmail.com>
2022-01-31 09:43:03 +0800

    Made user identity class

---
+++ Identity/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Budgeteer_REST.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string SettingsJSON { get; set; }
+        public string ProfileDir { get; set; }
+        public decimal TotalMoney { get; set; } = 0;
+    }
+}
