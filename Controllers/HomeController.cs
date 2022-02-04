b96e0d2fd825a9cf9531b48845780ee68fbea462
Towphe <castillochristopherjames@gmail.com>
2022-02-04 22:31:44 +0800

    Made HomeController class

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budgeteer_REST.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string GetHomeMessage()
+        {
+            return "Home";
+        }
+    }
+}
